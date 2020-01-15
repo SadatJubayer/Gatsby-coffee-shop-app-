2bac2a36e8e4563e52424941586d4b2e6f6130fe
S. M. Jubayer <sadatjubayer@gmail.com>
2020-01-16 02:21:05 +0600

    Initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import "./layout.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const Layout = ({ children }) => {
+  return <>{children}</>
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
